44d64b65dd96f677dad9537b5400228ea28e4dc3
FlynntKnapp <bruce.stull@gmail.com>
2023-10-16 08:02:49 -0400

    Move 'DEBUG' config setting to 'common.py'.

--- config/settings/production.py
+++ config/settings/production.py
@@ -20,23 +20,8 @@ DATABASES = {
     }
 }
 
-
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# Create a specific `SECRET_KEY` for production and use it in production
-# only.
-SECRET_KEY = os.environ.get("SECRET_KEY")
-
-# To create a new `SECRET_KEY`:
-"""
-    python manage.py shell
-    from django.core.management.utils import get_random_secret_key
-    print(get_random_secret_key())
-"""
